4229bb7f968288ab72f23167ddebe588bb38fd9f
Fabian Linzberger <e@lefant.net>
2014-03-02 12:32:38 +0100

    retrieve auth.php redirect url from config variable instead of hardcoding value

--- auth.php
+++ auth.php
@@ -31,6 +31,6 @@
     $_SESSION['flickr_token']  = $token;
     $_SESSION['flickr_message']  = $message;
     
-    header("Location: http://www.christophemaillot.fr/flickrdupfinder/");
+    header("Location: " . $home_url);
 
 ?>
